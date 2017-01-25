a7561a74c3516cbb5a73861226ceb37b80f3ebc0
West19 <ashanee.thompson@mycavehill.uwi.edu>
2017-01-25 11:46:23 -0400

    added header file

---
+++ Door.h
@@ -0,0 +1,25 @@
+
+
+#include <string>
+using namespace std;
+
+class Door
+{
+private:
+	string colour;
+	int width;
+	
+	
+
+	public:
+		Door();
+	void setColour(string color);
+	string getColour();
+
+	void setWidth(int wid);
+	int getWidth();
+	
+
+};
+
+
